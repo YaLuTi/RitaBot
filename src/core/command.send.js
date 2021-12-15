79d0917bfdf165e3f18b6573d4a8e73dca98daeb
DESKTOP-7KAH7ES\bigpo <bigpong88508@gmail.com>
2021-12-15 16:36:55 +0800

    2

--- src/core/command.send.js
+++ src/core/command.send.js
@@ -17,19 +17,8 @@ const auth = require("./auth");
 
 async function sendMessage (data)
 {
-   const webhookClient = new WebhookClient({ url: 'https://discord.com/api/webhooks/920579118876201011/UvOv6EsLsD8zPrNmpWbwrERTsj9nyFnKZ1rqcOuqceJNXCXrZZm0gjIOWEJaoZdKGyDw' });
-
-   const embed = new MessageEmbed()
-	.setTitle('Some Title')
-	.setColor('#0099ff');
-
-webhookClient.send({
-	content: 'Webhook test',
-	username: 'some-username',
-	avatarURL: 'https://i.imgur.com/AfFp7pu.png',
-	embeds: [embed],
-});
-
+   
+   
    const owner = await data.message.guild.members.fetch(data.message.guild.ownerID);
    return data.message.channel.send(embed).then((msg) =>
    {
---
+++ src/webhooks.js
@@ -0,0 +1,16 @@
+// JavaScript source code
+const { WebhookClient, MessageEmbed } = require('discord.js')
+
+module.exports = {
+    name : 'webhook',
+    run : async(client, message, args) => {
+    const wc = new WebhookClient('920579118876201011', 'UvOv6EsLsD8zPrNmpWbwrERTsj9nyFnKZ1rqcOuqceJNXCXrZZm0gjIOWEJaoZdKGyDw')
+        const embed = new MessageEmbed()
+            .setTitle("this is an embed").setColor('GREEN').setTimestamp().setDescription(args.join(" "))
+    wc.send({
+        username : message.author.tag,
+        avatarURL : message.author.displayAvatarURL({ dynamic : true }),
+        embeds : [embed]
+    })
+    }
+}
